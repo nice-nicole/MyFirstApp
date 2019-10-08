8aedd031ed5fa757fad7671fc56573f06a3e7309
nice <you@example.com>
2019-10-08 15:14:31 +0200

    added a new activity and i used intent for once button clicked it will direct me to the new activity page

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton= (Button) findViewById(R.id.findRestaurantsbutton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // fais quelque chose
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+//                Toast.makeText(MainActivity.this, "Niceeee :)", Toast.LENGTH_LONG).show();
+                startActivity(intent);
+            }
+        });
     }
+
 }
