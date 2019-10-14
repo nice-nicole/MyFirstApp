2f7b5f0bee6a07a2e7b4c2f6430443e631ccaf23
nice <you@example.com>
2019-10-14 19:57:53 +0200

    integration of api in the project

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -6,26 +6,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private Button mFindRestaurantsButton;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.findRestaurantsbutton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mFindRestaurantsButton= (Button) findViewById(R.id.findRestaurantsbutton);
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+        mFindRestaurantsButton.setOnClickListener(this);}
+
             @Override
             public void onClick(View v) {
-                // fais quelque chose
-                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-//                Toast.makeText(MainActivity.this, "Niceeee :)", Toast.LENGTH_LONG).show();
-                startActivity(intent);
-            }
-        });
-    }
+                if(v == mFindRestaurantsButton){
+                    String location= mLocationEditText.getText().toString();
+                    Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+        //                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
+                    intent.putExtra("location", location);
+                    startActivity(intent);
 
-}
+                }
+
+            }
+
+    }
+    
